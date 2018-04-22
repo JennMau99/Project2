94b8378412238b4733ed532a4f6f1fb6ea1b7131
JennMau99 <35314199+JennMau99@users.noreply.github.com>
2018-04-21 19:20:44 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+struct word {
+        char *word;
+        int frequency;
+};
+
+typedef struct word Word;
+
+static Word *hashtable[100];
+
+int main(int argc, char **argv)
+{
+        int wordnum = 10;
+        int filenames = 0;
+        if(argc > 1 && strcmp(argv[1], "-n"))
+        {
+                wordnum = argv[2][0];
+                if(argc > 2)
+                {
+                        filenames = 2;
+                }
+        }
+        else if(argc > 0)
+        {
+                filenames = 1;
+        }
+
+}
+
+void hash_words(int filenames, char **argv)
+{
