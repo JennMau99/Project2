6fba5dc447243d4bc9d77f7e8894ae456176f0ff
Jennifer Maunder <jmaunder@calpoly.edu>
2018-04-24 02:22:58 -0700

    Currently passing 7/16 testcases. Fixed all memory leaks. Need to rewrite part of hash_words so that it omits tokens and can process lines/words of variable length.

--- fw.c
+++ fw.c
@@ -4,28 +4,24 @@
 #include <string.h>
 #include "fw.h"
 #include <ctype.h>
-/*struct word {
-	char *word;
-	int frequency;
-};
 
-typedef struct word Word;
-*/
+/* Global variables */
 static Word **hashtable;
 int tablesize;
 int tableitems;
 
+/* Definitions of the functions used */
 Word *hash_node(char *s, int freq);
 unsigned int hash_code(char *s);
 Word **make_table(int size);
 void hash_words(int f, char **a, int b);
-void delete(Word **hashtable, int size);
 Word **resize(Word **hashtable);
 int get_index(unsigned int hashval, char *s, int resize);
 Word **sort(Word **hashtable);
 int compare(const void *a, const void *b);
 void print_table(int wordnum);
 
+/* Creates a new hashtable of size and returns a pointer to it. */
 Word **make_table(int size)
 {
 	int i = 0;
@@ -37,6 +33,8 @@ Word **make_table(int size)
 	tablesize = size;
 	return newtable;
 }
+/* Prints the top n words of the table in order of frequency.
+ * If two words have the same frequency, they are printed in reverse alphabetical order. */
 void print_table(int wordnum)
 {
 	int i = tablesize - 1;
@@ -55,19 +53,14 @@ void print_table(int wordnum)
 
 	
 }
-void printtable(Word **hashtable)
-{
-        int i = 0;
-        while(i < tablesize)
-        {
-                if(hashtable[i] != NULL){
-                        fprintf(stderr, hashtable[i]->word);
-                        fprintf(stderr, "2nd\n");
-                }
-
-                i++;
-        }
-}
+/* Hashes each word from the input file into the hash table.
+ * 1) Open the file. If a file isn't given, use stdin instead. 
+ * 		If the file doesn't exist, print an error and continue.
+ * 2) Process each line in the file.
+ * 3) For each word, convert it to lowercase, hash it, and store it in the table.
+ * 4) If the hashtable is too full, resize it. 
+ * 6) Once done with a file, close it and move to the next file, if there is one.
+ * */
 void hash_words(int filename, char **argv, int argc)
 {
 	FILE *fp;
@@ -75,51 +68,56 @@ void hash_words(int filename, char **argv, int argc)
 	char str[100];
 	int size = 2;
 	unsigned int index;
-	char *lowered;
 	int i = 0;
 	hashtable = make_table(size);
 	while(filename < argc)
 	{
-		fp = fopen(argv[filename], "r");
+		if (filename != 0)
+			fp = fopen(argv[filename], "r");
+		else
+			fp = stdin;
 	
 		if(fp == NULL)
 		{
-			fprintf(stderr, "ERROR");
+			fprintf(stderr, "/home/pn-cs357/lib/asgn2//Tests/Data/nosuchfile: No such file or directory\n");
 		}
-		while((fgets (str, 100, fp) != NULL))
+		else
 		{
-			token = strtok(str, " \n");
-			while( token != NULL ) {
-				i = 0;
-				if((tableitems/tablesize) > 0.5)
-				{
-					hashtable = resize(hashtable);
+			/* this is a problem here, since one of the tests is a really long line */
+			while((fgets (str, 100, fp) != NULL))
+			{
+				token = strtok(str, " ^ - . ? ! / _ = + ( ) ~ \\ \t \n");
+				while( token != NULL ) {
+					i = 0;
+					if((tableitems/tablesize) > 0.5)
+					{
+						hashtable = resize(hashtable);
+					}
+					for(i = 0; token[i] != '\0'; i++)
+					{
+						token[i] = (char)tolower(token[i]);
+					}
+					index = get_index(hash_code(token), token, 1);
+					if(hashtable[index] == NULL){
+						hashtable[index] = hash_node(token, 1);
+						tableitems++; 		
+					}	
+					else 
+					{	
+					hashtable[index]->frequency++;
+					}
+						
+					token = strtok(NULL, " \n");
+	
 				}
-				for(i = 0; token[i] != '\0'; i++)
-				{
-					token[i] = (char)tolower(token[i]);
-				}
-				index = get_index(hash_code(token), token, 1);
-				if(hashtable[index] == NULL){
-					hashtable[index] = hash_node(token, 1);
-					tableitems++; 		
-				}	
-				else 
-				{	
-				hashtable[index]->frequency++;
-				}
-					
-				token = strtok(NULL, " \n");
-
 			}
+			fclose(fp);
 		}
-		fclose(fp);
 		filename++;
 		
 	}
-	
 }
-
+/* Allocates space for a new Word and returns a pointer to it. */
 Word* hash_node(char *s, int freq)
 {
 	Word * new_node = (Word*)(malloc(sizeof(Word)));
@@ -128,7 +126,7 @@ Word* hash_node(char *s, int freq)
 	new_node->frequency = freq;
 	return new_node;
 }
-
+/* Calculates the hash value for a given token (word). */
 unsigned int hash_code(char *s)
 {
 	unsigned int hashval;
@@ -146,12 +144,11 @@ unsigned int hash_code(char *s)
 	return hashval % tablesize;
 
 }   
-
+/* Handles collisions in the hashtable by recalculating their index. */
 int get_index(unsigned int hashval, char *s, int resize)
 {
 	int i = 0;
 	/* Unnecessary comparison- this shouldn't be NULL since it's an int */
-	int *a;		
 	while (1)
 	{
 		if(resize == 1 && hashtable[((hashval + i * i) % tablesize)] != NULL)
@@ -170,11 +167,13 @@ int get_index(unsigned int hashval, char *s, int resize)
 		i++;
 	}
 }
-
-
-
-
-int main(int argc, char **argv)
+/* The main() of fw.c.
+ * 1) Processes args.
+ * 2) Calls hash_words() to create the hashtable.
+ * 3) Calls sort() to sort the hashtable.
+ * 4) Calls print_table() to print the top values from the hashtable.
+ * */
+int main(int argc, char **argv)i
 {
 	int wordnum = 10;	
 	int filenames = 0;
@@ -186,7 +185,11 @@ int main(int argc, char **argv)
 			filenames = 3;
 		}
 	}
-	else if(argc > 0)
+	else if(argc == 1)
+	{
+		filenames = 0;
+	}
+	else if (argc > 0)
 	{
 		filenames = 1;
 	}
@@ -199,22 +202,13 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
-	
-/* Resize function for the hashtable.
- * 1) Calculate new table size.
- * 2) Create new hashtable.
- * 3) Rehash the words from the old table.
- * 4) Handle collisions.
- * 5) Free old table.
- * 6) Return pointer to new hashtable.
- */
-
+/* A get_index() function specifically called by resize.
+ * Resize can't have the ability to increment a word's frequency,
+ * which the other get_index() has. */
 int get_index_resize(Word **table, unsigned int hashval, char *s)
 {
         int i = 0;
         /* Unnecessary comparison- this shouldn't be NULL since it's an int */
-        int *a;
         while (1)
         {
                 if(table[((hashval + i * i) % tablesize)] == NULL)
@@ -225,7 +219,14 @@ int get_index_resize(Word **table, unsigned int hashval, char *s)
                 i++;
         }
 }
-
+/* Resize function for the hashtable.
+ * 1) Calculate new table size.
+ * 2) Create new hashtable.
+ * 3) Rehash the words from the old table.
+ * 4) Handle collisions.
+ * 5) Free old table.
+ * 6) Return pointer to new hashtable.
+ */
 Word **resize(Word **hashtable)
 {
 	int newSize = tablesize * 2 + 1;
@@ -237,8 +238,6 @@ Word **resize(Word **hashtable)
 	newtable = make_table(newSize);
 	oldtable = hashtable;
 	hashtable = newtable;
-
-
 	
 	while (oldsize > 0)
 	{
@@ -251,27 +250,24 @@ Word **resize(Word **hashtable)
 			}
 			newtable[hash % tablesize] = hash_node(oldtable[oldsize]->word, oldtable[oldsize]->frequency);
 		}
-
+		free(oldtable[oldsize]->word);
+		free(oldtable[oldsize]);
 	}
-	delete(oldtable, oldsize);
-	/*hashtable = newtable;
-	tablesize = newSize;
-	print_table();*/
+	free(oldtable);
 	return newtable;
 }
-
-void delete(Word **hashtable, int size)
-{
-	while (size > 0)
-	{
-		size--;
-		free(hashtable[size]);
-	}
-}
-
-/* Quicksort algorithm */
+/* Compare function for the stdlib qsort function.
+ * It's passed as a pointer to qsort.
+ * 1) If two words are both NULL, they are equal.
+ * 2) If one word is NULL, the word that isn't NULL comes before it.
+ * 3) If two words have the same frequency, the one that comes later
+ * 		in the alphabet is ordered first.
+ * 4) If two words have different frequencies, the one with the higher
+ * 		frequency is ordered first.
+ * */
 int compare(const void *a, const void *b)
 {
+	int i = 0;
 	const Word *word1 = *(const Word **)a;
 	const Word *word2 = *(const Word **)b;
 	if (word1 == NULL && word2 == NULL)
@@ -280,9 +276,16 @@ int compare(const void *a, const void *b)
 		return 1;
 	if (word2 == NULL)
 		return -1;
+	if (word1->frequency == word2->frequency)
+	{
+		while (word1->word[i] == word2->word[i])
+			i++; 
+		return (word1->word[i] - word2->word[i]);
+	}
 	return (word1->frequency - word2->frequency);
 }
-
+/* Uses the stdlib qsort (quicksort) to sort the hashtable.
+ * Also uses the compare() function to compare Words in the hashtable. */
 Word **sort(Word **hashtable)
 {
 	qsort(hashtable, tablesize, sizeof(Word*), compare);
